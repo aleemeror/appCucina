a5da24e60259bcb46345e4cd042d8c375665c58c
Cavazzini <andrea.cavax@hotmail.com>
2016-05-22 15:53:41 +0200

    Fixata seekbar

--- AppCucinaProject/app/src/main/java/com/example/studente/appcucinaproject/RicercaAvanzata/RicercaAV.java
+++ AppCucinaProject/app/src/main/java/com/example/studente/appcucinaproject/RicercaAvanzata/RicercaAV.java
@@ -2,6 +2,7 @@ package com.example.studente.appcucinaproject.RicercaAvanzata;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.NumberPicker;
 
 import com.example.studente.appcucinaproject.CalcolaTeglia.CalcolaTeglia;
@@ -27,7 +30,12 @@ import com.example.studente.appcucinaproject.Timer.Timer;
 public class RicercaAV extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-    private NumberPicker pickerOre, pickerMinuti,pickerSecondi;
+    //private NumberPicker pickerOre, pickerMinuti,pickerSecondi;
+    private EditText nomeRicerca;   //editText
+    private EditText ingrediente1,ingrediente2,ingrediente3; //2,3,4
+    private CheckBox antipastoCB, primoCB, secondoCB, dolceCB;
+
+    private SQLiteDatabase mydatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +43,6 @@ public class RicercaAV extends AppCompatActivity
         setContentView(R.layout.activity_ricerca_av);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -52,7 +58,22 @@ public class RicercaAV extends AppCompatActivity
         //ROTAZIONE SCHERMO BLOCCATA
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        pickerOre = (NumberPicker) findViewById(R.id.numberPickerHours);
+        nomeRicerca = (EditText)findViewById(R.id.editText);
+        ingrediente1 = (EditText)findViewById(R.id.editText2);
+        ingrediente2 = (EditText)findViewById(R.id.editText3);
+        ingrediente3 = (EditText)findViewById(R.id.editText4);
+
+        antipastoCB = (CheckBox)findViewById(R.id.checkBox);
+        primoCB = (CheckBox)findViewById(R.id.checkBox2);
+        secondoCB = (CheckBox)findViewById(R.id.checkBox3);
+        dolceCB = (CheckBox)findViewById(R.id.checkBox4);
+
+
+        mydatabase = openOrCreateDatabase("your database name",MODE_PRIVATE,null);
+
+
+
+        /*pickerOre = (NumberPicker) findViewById(R.id.numberPickerHours);
         pickerOre.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
         pickerOre.setMaxValue(99);
         pickerOre.setMinValue(00);
@@ -75,7 +96,7 @@ public class RicercaAV extends AppCompatActivity
 
         pickerOre.setValue(0);
         pickerMinuti.setValue(0);
-        pickerSecondi.setValue(0);
+        pickerSecondi.setValue(0);*/
 
 
 
